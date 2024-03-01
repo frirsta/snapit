27cf9c326ae59c157a70af70c8fae03a65212902
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-03-01 12:22:58 +0100

    Add new baseURL

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://snapitapi-a9c9b5ffecd8.herokuapp.com/'
+axios.defaults.baseURL = 'https://snapitapi.netlify.app/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true
 
